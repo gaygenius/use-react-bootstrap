e5f92ba31a9f7f1bda6ffd5f13339918129401ed
Jose Capo <jose@josecapo.com>
2019-12-28 14:07:23 -0800

    :wrench: Add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +21,21 @@ function App() {
         >
           Learn React
         </a>
+        <Alert show={show} variant="success">
+          <Alert.Heading>How's it going?!</Alert.Heading>
+          <p>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula,
+            eget lacinia odio sem nec elit. Cras mattis consectetur purus sit
+            amet fermentum.
+          </p>
+          <hr />{" "}
+          <div className="d-flex justify-content-end">
+            <Button onClick={() => setShow(false)} variant="outline-success">
+              Close me ya'll!
+            </Button>
+          </div>
+        </Alert>
+        {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
       </header>
     </div>
   );

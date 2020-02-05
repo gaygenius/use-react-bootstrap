f483f40f632be99e0eaeedf69dfb7f41c5b6e78a
Jose Capo <jose@josecapo.com>
2020-02-04 17:08:10 -0800

    Updates

---
+++ precache-manifest.fa5e60e9e64345c37b45161d44a34921.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201104d9cceafca79b0e316ad3be812a",
+    "url": "/use-react-bootstrap/index.html"
+  },
+  {
+    "revision": "244b9dbe13e4db2b93f1",
+    "url": "/use-react-bootstrap/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b2e1c5f5f81704bc3166",
+    "url": "/use-react-bootstrap/static/js/2.19fb4c3a.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/use-react-bootstrap/static/js/2.19fb4c3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "244b9dbe13e4db2b93f1",
+    "url": "/use-react-bootstrap/static/js/main.e991f67a.chunk.js"
+  },
+  {
+    "revision": "e68ec1db74bf4b7d1678",
+    "url": "/use-react-bootstrap/static/js/runtime-main.b8028f23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/use-react-bootstrap/static/media/logo.5d5d9eef.svg"
+  }
+]);
